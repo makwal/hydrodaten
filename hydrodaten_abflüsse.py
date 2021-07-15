8f5d68a173eae7ae3f346dad2fda97dc25291506
root <ma-wa@gmx.ch>
2021-07-15 16:03:55 +0200

    auto-update

--- hydrodaten_abflüsse.py
+++ hydrodaten_abflüsse.py
@@ -11,20 +11,74 @@ from datetime import timedelta
 # In[ ]:
 
 
-def abfluss(station):
+def abfluss(station, gefahrenstufen):
     df_abfluss = pd.read_csv('{}.csv'.format(station))
     df_abfluss['Time'] = pd.to_datetime(df_abfluss['Time'])
     df_abfluss['date'] = pd.to_datetime(df_abfluss['Time'].dt.strftime('%Y-%m-%d %H:%M:%S')) + timedelta(hours=1)
     df_abfluss.columns = ['Time', 'abfluss', 'date']
     df_abfluss = df_abfluss[['date', 'abfluss']].copy()
-    df_abfluss.to_csv('final_data/final_{}.csv'.format(station))
+    df_abfluss['gs1'] = gefahrenstufen['gs1']
+    df_abfluss['gs2'] = gefahrenstufen['gs2']
+    df_abfluss['gs3'] = gefahrenstufen['gs3']
+    df_abfluss['gs4'] = gefahrenstufen['gs4']
+    df_abfluss['gs5'] = gefahrenstufen['gs5']
+    df_abfluss.to_csv('final_data/final_{}.csv'.format(station), index=False)
 
 
-# In[ ]:
+# In[7]:
 
 
-stations = [2016, 2018, 2063, 2091, 2110, 2243]
+stations = {
+    2016:
+    {
+        'gs1': 820,
+        'gs2': 1100,
+        'gs3': 1250,
+        'gs4': 1350,
+        'gs5': 2000
+    },
+    2018:
+    {
+        'gs1': 480,
+        'gs2': 640,
+        'gs3': 720,
+        'gs4': 830,
+        'gs5': 2000
+    },
+    2063:
+    {
+        'gs1': 720,
+        'gs2': 940,
+        'gs3': 1050,
+        'gs4': 1150,
+        'gs5': 2000
+    },
+    2091:
+    {
+        'gs1': 2500,
+        'gs2': 3000,
+        'gs3': 3600,
+        'gs4': 4500,
+        'gs5': 6000
+    },
+    2110:
+    {
+        'gs1': 470,
+        'gs2': 600,
+        'gs3': 700,
+        'gs4': 810,
+        'gs5': 2000
+    },
+    2243:
+    {
+        'gs1': 350,
+        'gs2': 480,
+        'gs3': 550,
+        'gs4': 630,
+        'gs5': 2000
+    }
+}
 
-for s in stations:
-    abfluss(str(s))
+for key, value in stations.items():
+    abfluss(str(key), value)
 
